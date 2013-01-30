f6e00917c01062d76cd84156f73c63f708b9a3b9
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-30 18:24:19 -0200

    sometimes path needs a /

--- lib/remote-terminal/cli.rb
+++ lib/remote-terminal/cli.rb
@@ -13,10 +13,26 @@ module RemoteTerminal
     def CLI.execute(cmd, rsync, ssh, path)
       puts path
       project = Project.find(path)
-      output = rsync.run(project.path_from(path), "#{project.user}@#{project.ip}:#{project.remote_directory}")
+      output = rsync.run(project.path_from(path), without_slash("#{project.user}@#{project.ip}:#{project.remote_directory}"))
       output += ssh.run("#{project.user}@#{project.ip}", File.join(project.remote_directory, project.path_to(path)), cmd)
-      output += rsync.run("#{project.user}@#{project.ip}:#{project.remote_directory}/", project.path_from(path))
+      output += rsync.run(with_slash("#{project.user}@#{project.ip}:#{project.remote_directory}"), project.path_from(path))
       return output
+    end
+    
+    def CLI.without_slash(path)
+      if path[-1] == '/'
+        return path[0..-2]
+      else
+        return path
+      end
+    end
+    
+    def CLI.with_slash(path)
+      if path[-1] == '/'
+        return path
+      else
+        return "#{path}/"
+      end
     end
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -15,7 +15,7 @@ module RemoteTerminal
           and_return("rsync ")
           
       rsync.should_receive(:run).
-        with('igorbonadio@10.210.123.4:~/Projects/MyProject', '.').
+        with('igorbonadio@10.210.123.4:~/Projects/MyProject/', '.').
           and_return("rsync ")
           
       CLI.execute('make', rsync, ssh, 'features/data/project/')
@@ -34,7 +34,7 @@ module RemoteTerminal
           and_return("rsync ")
           
       rsync.should_receive(:run).
-        with('igorbonadio@10.210.123.4:~/Projects/MyProject', '../..').
+        with('igorbonadio@10.210.123.4:~/Projects/MyProject/', '../..').
           and_return("rsync ")
           
       CLI.execute('make', rsync, ssh, 'features/data/project/folder/subfolder/')
