9fd6974fcd7bca2ca2b999eb14a3494d710a0520
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-30 17:11:20 -0200

    changing yml file

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module RemoteTerminal
   describe Project do
     it "should get project's configuration" do
-      config = YAML::load(File.open('tmp/.remote-terminal.yml'))
-      project = Project.find('tmp')
+      config = YAML::load(File.open('features/data/project/.remote-terminal.yml'))
+      project = Project.find('features/data/project')
       project.ip.should be == config['ip']
       project.user.should be == config['user']
       project.remote_directory.should be == config['remote_directory']
