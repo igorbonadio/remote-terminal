abf9721780c3990eae129783c1fe05bf5fe63024
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-30 17:45:14 -0200

    rt-config app

--- lib/remote-terminal.rb
+++ lib/remote-terminal.rb
@@ -1,5 +1,7 @@
 ROOT_PATH = File.expand_path(File.dirname(__FILE__))
 
+require 'yaml'
+
 require File.join(ROOT_PATH, '/remote-terminal/cli')
 require File.join(ROOT_PATH, '/remote-terminal/project')
 require File.join(ROOT_PATH, '/remote-terminal/rsync')
