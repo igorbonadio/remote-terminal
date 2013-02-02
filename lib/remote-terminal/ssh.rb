45929cf59b3dfd2df558cddc6a49db5228e63d36
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-02 12:50:48 -0200

    rt shows output when it is generated

--- lib/remote-terminal/ssh.rb
+++ lib/remote-terminal/ssh.rb
@@ -2,8 +2,13 @@ module RemoteTerminal
   class SSH
     def run(server, remote_directory, cmd)
       IO.popen("ssh #{server} 'cd #{remote_directory} && #{cmd}'") do |f|
-        return "#{f.readlines.join()}"
+        output = f.gets
+        while output
+          puts output
+          output = f.gets
+        end
       end
+      return ""
     end
   end
 end
